9bf8147d03ada974c2cb909a13a784f50f61f93e
luccaa79 <luca.vatrella@rmdit.ch>
2026-05-20 10:37:59 +0200

    Add files via upload

---
+++ moodle-migration/config.php
@@ -0,0 +1,27 @@
+<?php  
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mariadb';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'db';
+$CFG->dbname    = 'moodle';
+$CFG->dbuser    = 'vmadmin';
+$CFG->dbpass    = 'Riethuesli>12345';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_unicode_ci',
+);
+
+$CFG->wwwroot   = 'http://localhost:80';
+$CFG->dataroot  = '/var/www/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
